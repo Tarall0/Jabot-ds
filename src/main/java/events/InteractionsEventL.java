e75b7d992e421de92e2f0e48955448363f439038
Tarallo <98182571+Tarall0@users.noreply.github.com>
2023-07-18 19:22:40 +0200

    getting gild info clean

--- src/main/java/events/InteractionsEventL.java
+++ src/main/java/events/InteractionsEventL.java
@@ -4,7 +4,7 @@ import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
-import org.jetbrains.annotations.NotNull;
+
 
 
 public class InteractionsEventL extends ListenerAdapter{
