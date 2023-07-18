f0fd891124f82409d6f33479bd29b2159edec104
Tarallo <98182571+Tarall0@users.noreply.github.com>
2023-07-18 23:11:22 +0200

    some fixes x

--- src/main/java/events/MessageEventL.java
+++ src/main/java/events/MessageEventL.java
@@ -1,17 +1,16 @@
 package events;
 
-import net.dv8tion.jda.api.Permission;
+
 import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.entities.emoji.Emoji;
-import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
-import net.dv8tion.jda.api.events.thread.member.ThreadMemberJoinEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
 
 public class MessageEventL extends ListenerAdapter {
     @Override
-    public void onMessageReceived(MessageReceivedEvent event) {
+    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
         super.onMessageReceived(event);
         if (!event.getAuthor().equals(event.getJDA().getSelfUser())){
             if (event.getMessage().getContentRaw().contains("weed")) {
@@ -24,7 +23,7 @@ public class MessageEventL extends ListenerAdapter {
 
 
     @Override
-    public void onMessageReactionAdd(MessageReactionAddEvent event) {
+    public void onMessageReactionAdd(@NotNull MessageReactionAddEvent event) {
         super.onMessageReactionAdd(event);
 
         if (!event.getUser().equals(event.getJDA().getSelfUser())){
