711eaad6569c70562acc2051b66d135d35302242
Tarallo <98182571+Tarall0@users.noreply.github.com>
2023-07-22 08:03:44 +0200

    added try catches to handle some exceptions

--- src/main/java/events/MessageEventL.java
+++ src/main/java/events/MessageEventL.java
@@ -1,7 +1,8 @@
 package events;
 
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
@@ -9,9 +10,12 @@ import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Random;
+import java.util.*;
 
 public class MessageEventL extends ListenerAdapter {
+
+    private final Set<String> bannedWords = new HashSet<>(Arrays.asList("nigga", "whore", "faggot", "https://", "www.")); // Add your banned words here
+
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
         super.onMessageReceived(event);
@@ -35,6 +39,29 @@ public class MessageEventL extends ListenerAdapter {
                 event.getMessage().getAuthor().getAsMention();
             }
         }
+
+        // Get the user's roles
+        List<Role> userRoles = (List<Role>) event.getMember().getRoles();
+
+        // Check for banned words based on user roles
+        if (userRoles.isEmpty()) {
+            checkBannedWords(event.getMessage(), "DEFAULT");
+        } else {
+            for (Role role : userRoles) {
+                if(role.getName().equalsIgnoreCase("white role")){
+                    return;
+                }
+                if (role.getName().equalsIgnoreCase("Moderator")) {
+                    // Moderators have no word filtering, but you can add other actions here if needed
+                    return;
+                } else if (role.getName().equalsIgnoreCase("Admin")) {
+                    // Admins have no word filtering, but you can add other actions here if needed
+                    return;
+                }
+            }
+            // If the user has roles but is not an Admin or Moderator, check for banned words
+            checkBannedWords(event.getMessage(), "USER");
+        }
         System.out.println(event.getMessage().getAuthor().getName() + " sent '" + event.getMessage().getContentDisplay() + "'");
     }
 
@@ -43,25 +70,44 @@ public class MessageEventL extends ListenerAdapter {
     public void onMessageReactionAdd(@NotNull MessageReactionAddEvent event) {
         super.onMessageReactionAdd(event);
 
-        try{
-            if (!event.getUser().equals(event.getJDA().getSelfUser())){
-                String username = event.getUser().getGlobalName();
-                String emoji = event.getReaction().getEmoji().getAsReactionCode();
-                String channelName = event.getChannel().getAsMention();
 
-                String message = username + " reacted to a message with " +" "+ emoji + " "+ " in "+channelName;
+        if (!Objects.equals(event.getUser(), event.getJDA().getSelfUser())){
+            String username = Objects.requireNonNull(event.getUser()).getGlobalName();
+            String emoji = event.getReaction().getEmoji().getAsReactionCode();
+            String channelName = event.getChannel().getAsMention();
 
-                try{
-                    event.getGuild().getDefaultChannel().asTextChannel().sendMessage(message).queue();
-                }catch (NullPointerException e){
-                    System.out.println(e);
-                }
+            String message = username + " reacted to a message with " +" "+ emoji + " "+ " in "+channelName;
 
+            try{
+                event.getGuild().getDefaultChannel().asTextChannel().sendMessage(message).queue();
+            }catch (NullPointerException e){
+                System.out.println(e);
             }
-        } catch (NullPointerException e){
-            System.out.println(e);
+
         }
 
+
+    }
+
+    private void checkBannedWords(Message message, String userGroup) {
+        String content = message.getContentRaw().toLowerCase();
+        for (String bannedWord : bannedWords) {
+            if (content.contains(bannedWord)) {
+                // Depending on the user group, you can choose the appropriate moderation action here
+                switch (userGroup) {
+                    case "DEFAULT":
+                        // For default users, you can delete the message and warn them, or take other actions
+                        message.delete().queue();
+                        message.getChannel().sendMessage("Your message contains inappropriate content and has been deleted. Please review our rules. ").queue();
+                        break;
+                    case "USER":
+                        // For users with roles, you can choose a different moderation action here
+                        // For example, you may want to notify moderators or log the incident
+                        break;
+                }
+                break; // Exit the loop after finding one banned word
+            }
+        }
     }
 
     @Override
