6c6d2a94e693984d98b133317cdeb7fa4b3653db
Tarallo <98182571+Tarall0@users.noreply.github.com>
2023-08-23 13:08:18 +0200

    cleaning code

--- src/main/java/events/SpinWheel.java
+++ src/main/java/events/SpinWheel.java
@@ -10,7 +10,6 @@ import org.jetbrains.annotations.NotNull;
 
 import java.sql.SQLException;
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class SpinWheel extends ListenerAdapter {
     private final DatabaseManager databaseManager;
